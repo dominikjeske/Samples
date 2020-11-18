75770f41ae5cd6b16d9c701d3d70737fe2e6daa3
Dominik Jeske <dominik.jeske@hotmail.com>
2020-11-18 20:37:39 +0100

    Dotnetformat

---
+++ DotnetFormat/ConsoleApp8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using System;
+
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+
+
+
+
+        static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            return Task.CompletedTask;
+        }
+    }
+}
