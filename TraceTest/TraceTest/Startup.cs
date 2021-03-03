82de793c0192ff3c9c0406136d66cc111de10be8
Dominik Jeske <dominik.jeske@partners.mbank.pl>
2021-03-03 15:15:47 +0100

    Trace Test

---
+++ TraceTest/TraceTest/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Compact;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraceTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHttpContextAccessor();
+            services.AddHttpClient();
+
+            Log.Logger = new LoggerConfiguration()
+                            .WriteTo.Console()
+                            .WriteTo.File(new CompactJsonFormatter(), @"C:\temp\testlog\log.json", shared: true)
+                            .MinimumLevel.Error()
+                            .CreateLogger();
+
+            services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ TraceTest/TraceTestNode2/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TraceTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IHttpClientFactory httpClientFactory, IHttpContextAccessor contextAccessor)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+            _contextAccessor = contextAccessor;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            _logger.LogError("Test on Node 2 with {@Headers}", _contextAccessor.HttpContext.Request.Headers);
+
+            var client = _httpClientFactory.CreateClient();
+            var result = await client.GetAsync("http://localhost:5020/WeatherForecast?test=three");
+
+            var content = await result.Content.ReadAsStringAsync();
+
+            return $"Result from node: {content}";
+        }
+    }
+}
